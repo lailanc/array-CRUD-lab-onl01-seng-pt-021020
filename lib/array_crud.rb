b690e8e81d5db67ee6f231fc592b29df02fa0638
Laila Campbell <lailacampbell96@gmail.com>
2020-02-15 22:33:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ["arrays!"]
+  .unshift("arrays!")
 end
 
 def remove_element_from_end_of_array(array)
